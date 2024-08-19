65bfa6e6e673c51d9d810da2081815287f159341
Akash-guna <akashguna6789@gmail.com>
2024-08-19 10:21:18 +0000

    undo last commit

--- factory.py
+++ factory.py
@@ -7,7 +7,7 @@ from nyuntam.constants.keys import (
     JobServices,
 )
 from nyuntam.utils.logger import set_logger
-from nyuntam.algorithm import Algorithm, VisionAlgorithm
+from nyuntam.algorithm import Algorithm
 from typing import Dict, Optional, Union, List
 from abc import abstractmethod
 from pathlib import Path
@@ -163,7 +163,5 @@ class Factory:
     def run(self) -> None:
         if self.algorithm is None:
             raise ValueError("No algorithm instance has been created.")
-        if issubclass(type(self.algorithm), VisionAlgorithm):
-            self.algorithm.compress_model()
-        else:
-            self.algorithm.run()
+
+        self.algorithm.run()
