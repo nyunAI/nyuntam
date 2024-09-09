abea93201b34c77e8bd09c13499042037fe11eab
Akash-guna <akashguna6789@gmail.com>
2024-09-09 10:40:58 +0000

    adapt,textgen setuptools

--- setup.py
+++ setup.py
@@ -52,8 +52,10 @@ visionbase = [
 onnx = ["onnx==1.15.0", "onnxruntime==1.16.3"]
 nncf = onnx + ["openvino==2023.2.0", "openvino-telemetry==2023.2.1", "nncf==2.7.0"]
 tensorrt = onnx + ["tensorrt==8.5.2", "pycuda"]
-openmm = ["openmim==0.3.9", "mmengine==0.10.2", "mmcv==2.2.0"]
 
+torchprune = ["torch-pruning==1.3.2"]
+
+openmm = ["openmim==0.3.9", "mmengine==0.10.2", "mmcv==2.2.0"]
 mmdet = openmm + ["mmdet==3.3.0"]  # for now
 mmseg = openmm + ["mmsegmentation==1.2.2"]
 mmpose = openmm + ["mmpose==1.3.1"]
@@ -78,7 +80,36 @@ classification = classification_datasets + classification_modelloading
 pose = od_dataset + mmpose
 segmentation = od_dataset + mmseg
 quantization = nncf + onnx + tensorrt
-dependency_links = ["https://download.pytorch.org/whl/cu121"]
+
+
+adapt_base = [
+    "peft==0.11.1",
+    "bitsandbytes==0.43.1",
+    "sentence-transformers==2.2.2",
+    "sentencepiece==0.2.0",
+    "transformers==4.40.1",
+    "triton==2.1.0",
+    "trl==0.8.6",
+]
+accelerate = ["accelerate==0.29.3"]
+
+textgen_base = ["transformers==4.40.1", "datasets==2.19.0", "sentencepiece==0.2.0"]
+autoawq = ["autoawq", "autoawq_kernels"]
+
+aqlm = accelerate + [
+    "safetensors==0.4.3",
+]
+
+flap = [
+    "packaging",
+    "flash_attn",
+] + adapt_base
+
+tensorrtllm = ["mpi4py", "tensorrt-llm"]
+qserve = ["Qserve"]
+
+
+dependency_links = ["https://download.pytorch.org/whl/cu121", "https://pypi.nvidia.com"]
 
 setup(
     name="nyuntam",
@@ -89,18 +120,34 @@ setup(
     install_requires=install_requires,
     dependency_links=dependency_links,
     extras_require={
+        # vision
         "object_detection": process(visionbase + object_detection),
         "classification": process(visionbase + classification),
         "pose": process(visionbase + pose),
+        "segmentation": process(visionbase + segmentation),
+        "mmadapt": proecess(adaptbase + mmpose + mmseg + mmdet),
         "nncf": process(visionbase + nncf),
         "tensorrt": process(visionbase + tensorrt),
         "onnx": process(visionbase + onnx),
-        "quantization": process(visionbase + quantization),
+        "torchprune": process(visionbase + torchprune),
         "mmrazor": process(visionbase + mmrazor),
         "mmdeploy": process(visionbase + mmdeploy),
         "objectdetectionstack": process(
             visionbase + object_detection + quantization + mmrazor + mmdeploy
         ),
         "classificationstack": process(visionbase + classification + quantization),
+        # adapt
+        "adapt": adaptbase,
+        "adaptmm": process(adaptbase + mmpose + mmseg + mmdet),
+        "accelerate": accelerate,
+        # text-generation
+        "text-gen": process(
+            textgen_base + flap + tensorrtllm + qserve + autoawq + aqlm
+        ),
+        "flap": process(textgen_base + flap),
+        "tensorrtllm": process(textgen_base + tensorrtllm),
+        "aqlm": process(textgen_base + aqlm),
+        "autoawq": process(textgen_base + autoawq),
+        "qserve": process(textgen_base + qserve),
     },
 )
